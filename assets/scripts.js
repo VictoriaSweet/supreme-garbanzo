49410ddcc6b3f0db6eac90090dce0303b552230b
VictoriaSweet <sweet.victoria1218@gmail.com>
2023-09-14 18:11:15 -0500

    added cards to index

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,5 @@
-";
+const apiKey = "8db965459e689988c56f9bc2eafac014";
+
 //prompt user input for city search
 const city = "Austin";
 const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
