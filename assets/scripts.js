9f7dfb36b878f9a3f29f8727c8ca042ae4d763c9
Victoria <sweet.victoria1218@gmail.com>
2023-09-13 21:55:56 -0500

    featching weather URL and added bootstrap

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,15 @@
 const apiKey = "8db965459e689988c56f9bc2eafac014";
+//prompt user input for city search
+const city = "Austin";
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-// https://api.openweathermap.org/data/2.5/forecast?q=Austin&appid=8db965459e689988c56f9bc2eafac014&units=imperial
-const weatherApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+
+fetch(url).then((response) => {
+    //going to be a json string 
+    //parse the json
+
+    response.json ().then (weatherData => {
+        console.log(weatherData);
+    })
+});
