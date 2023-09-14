7784e7751822e1ceb5b1768cf6c182f0269b0946
Victoria <sweet.victoria1218@gmail.com>
2023-09-13 19:35:02 -0500

    corrected API link information

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,4 @@
-const weatherApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=$8db965459e689988c56f9bc2eafac014`;
+const apiKey = "8db965459e689988c56f9bc2eafac014";
+
+// https://api.openweathermap.org/data/2.5/forecast?q=Austin&appid=8db965459e689988c56f9bc2eafac014&units=imperial
+const weatherApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
